d019571eaf9224319fb53de0c6cd1f314d641796
David van Oudheusden <doudheusden@novacollege.nl>
2023-05-09 13:40:33 +0200

    migraties aangemaakt

--- database/migrations/2023_05_09_111806_create_comments_table.php
+++ database/migrations/2023_05_09_111806_create_comments_table.php
@@ -11,15 +11,16 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('comments', function (Blueprint $table) {
             $table->id();
-            $table->string('firstname');
-            $table->string('lastname');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->string('role')->default('user');
-            $table->rememberToken();
+            $table->string('text');
+            
+            $table->unsignedBigInteger('user_id');
+            $table->unsignedBigInteger('post_id');
+
+
+            $table->foreign('post_id')->references('id')->on('posts');
+            $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
         });
     }
@@ -29,6 +30,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('comments');
     }
 };
